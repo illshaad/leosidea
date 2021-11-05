8544e638e67bcaa3e4747d34a0c4f958f5b0edc8
unknown <illshaad.budureea@avec.fr>
2021-11-05 15:13:38 +0100

    finish projet

--- back-end/controller/user-ctrl.js
+++ back-end/controller/user-ctrl.js
@@ -1,12 +1,16 @@
 const User = require("../model/user");
 
-saveUserToBDD = async (req, res) => {
-  console.log(req.body);
+const saveUserToBDD = async (req, res) => {
   const user = new User({
     ...req.body,
   });
   user.save();
-  res.status(200).json(user);
+  res.status(200).json("Vous êtes bien enregistré dans la base de donnée");
 };
 
-module.exports = { saveUserToBDD };
+const getUser = async (req, res) => {
+  const users = await User.find({});
+  res.status(200).json(users);
+};
+
+module.exports = { saveUserToBDD, getUser };
--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controller/user-ctrl");
+const userGetCtrl = require("../controller/user-ctrl");
 
 router.post("/user", userCtrl.saveUserToBDD);
+router.get("/user/users", userGetCtrl.getUser);
 
 module.exports = router;
